04a84a924159b272aef2007085cbf4b008db77f8
dii <dstephenhono@gmail.com>
2020-06-02 15:37:12 -0400

    testing insert function.

--- tests/rbtree-tests.c
+++ tests/rbtree-tests.c
@@ -1,26 +1,38 @@
 #include "rbtree.h"
 #include<stdio.h>
 
+void testOne();
+void printTree(struct RBTreeNode*);
+
 int main(int argc, char** argv) {
 
   printf("Beginning Red-Black Tree Tests!\n");
-
-  /* Create nodes. */
-  struct RBTreeNode *root  = init_rbtree(0, NULL);
-  struct RBTreeNode *child = init_rbtree_node(root, NULL, NULL, 1, NULL, BLACK);
-
-  root->left = child;
-
-  printf("[%p]: RBTreeNode root\n", root);
-  printf("\t[%p]: RBTreeNode root->left\n", root->left);
-  printf("\t\t[%p]: RBTreeNode root->left->parent\n", (root->left)->parent);
-  printf("\t[%p]: RBTreeNode root->right\n", root->right);
-  printf("[%p]: RBTreeNode child\n", child);
-
-  printf("Destroying nodes!\n");
-  dest_rbtree_node(child);
-  dest_rbtree_node(root);
-  printf("Done!\n");
+  testOne();
 
   return 0;
+}
+
+void testOne() {
+
+  struct RBTreeNode *root = init_rbtree(0, NULL);
+  insert(10, NULL, &root);
+  insert(4, NULL, &root);
+  insert(20, NULL, &root);
+
+  printTree(root);
+
+}
+
+void printTree(struct RBTreeNode* root) {
+
+  /* Inorder traversal of the tree. */ 
+
+  if (root != NULL) {
+    printTree(root->left);
+    printf("[%p] Key = %d\n", root, root->key);
+    printTree(root->right);
+
+  }
+
+
 }
