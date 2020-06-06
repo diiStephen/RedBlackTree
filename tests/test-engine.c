169ab0aae8d2f1f91e7586fb37cf0f3ff1d8525b
dii <dstephenhono@gmail.com>
2020-06-06 17:11:15 -0400

    added test-engine program.

---
+++ tests/test-engine.c
@@ -0,0 +1,38 @@
+#include "rbtree.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*
+
+Test engine for rbtree library. Commands accepted from stdin
+for building an RBT and performing queries. As commands are accepted
+from stdin, redirection can be used to create test files.
+
+As this is for test purposes, only ints are allowed as
+parameters to commands.
+
+Accepted commads and their formats:
+
+1. ins x -- insert x into the tree.
+
+2. del x -- remove x from the tree.
+
+3. srh x -- search for x in tree.
+
+4. max -- display max key in the tree.
+
+5. min -- display min key in the tree. 
+
+6. end -- shutdown the test program.
+
+*/
+
+int main(int argc, char** argv) {
+
+  printf("Starting test engine!\n");
+  char* cmd =
+
+
+  return 0;
+}
